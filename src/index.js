0af733945480d03128728eab7567eabb6faf50c5
Lauren <laurendoan7@gmail.com>
2023-08-29 20:38:21 -0500

    Display current time in locale time string

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 const getCurrentTime = () => {
     let dtNow = new Date();
-    let intHours = dtNow.getHours();
-    // console.log('Hours: ' + intHours);
 
-    let intMins = dtNow.getMinutes();
-    // console.log('Mins: ' + intMins);
-
-    let intSecs = dtNow.getSeconds();
-    // console.log('Secs: ' + intSecs);
-
-    let strTime = intHours + ':' + intMins + ':' + intSecs;
-    // console.log('Time: ' + strTime);
-
-    document.getElementById('clock').innerText = strTime;
+    document.getElementById('clock').innerText = dtNow.toLocaleTimeString();
 };
 
 // Get the current time every second
