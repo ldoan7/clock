ac5e5aace27528e44bf94ba4d3da4629d12bb56c
Lauren <laurendoan7@gmail.com>
2023-08-29 20:56:46 -0500

    Displays the current time zone and offset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+/**
+ * Gets and displays the current time.
+ */
 const getCurrentTime = () => {
     let dtNow = new Date();
 
-    document.getElementById('clock').innerText = dtNow.toLocaleTimeString();
+    // Get time zone offset
+    let intTimeZoneOffset = dtNow.getTimezoneOffset() / 60;
+    let strSign = (intTimeZoneOffset > 0) ? '-' : '+';
+
+    // Get time zone
+    let strTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    document.getElementById('clock').innerText = dtNow.toLocaleTimeString() + ' UTC' + strSign +
+        intTimeZoneOffset + ' (' + strTimeZone + ')';
 };
 
 // Get the current time every second
